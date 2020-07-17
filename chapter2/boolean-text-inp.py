a5e3ae23b60b59648bc5e2cc71d5c1dbd257a6a7
explodinghat <35501013+explodinghat@users.noreply.github.com>
2020-07-17 15:14:15 +0100

    commit

---
+++ chapter2/boolean-text-inp.py
@@ -0,0 +1,3 @@
+server = input('Enter \'s\' or \'server\':')
+if server == 'server' or 's':
+    server = True
---
+++ chapter2/while-text-inp.py
@@ -0,0 +1,7 @@
+while True:
+    print("Enter 's' for 'Server'")
+    server = input()
+    if server != 's':
+        continue
+    print("Congrats! You entered 's'")
+    break
